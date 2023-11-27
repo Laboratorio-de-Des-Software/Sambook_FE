9710e8b2a23aa30095a75401260fc9df4d718adb
rafael <rcborges98@gmail.com>
2023-11-26 22:40:03 -0300

    chore: add min height

--- src/MainTemplate.tsx
+++ src/MainTemplate.tsx
@@ -4,7 +4,7 @@ import SideBar from './components/SideBar/SideBar'
 
 const MainTemplate = () => {
   return (
-    <main className="relative">
+    <main className="relative min-h-screen">
       <Header />
       <SideBar />
       <section className="ml-[200px] h-full p-5 bg-[#FFFFFF]">
